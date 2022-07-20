c5736cfb3d6b785be79465cc668eacc9a1682c67
klaipher <klaipher@users.noreply.github.com>
2022-07-20 14:08:33 +0000

    ✅ Ready to clone and code.

--- fastapi_test_project/app.py
+++ fastapi_test_project/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+fastapi_test_project API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="fastapi_test_project",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://fastapi_test_project.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "klaipher",
+        "url": "http://fastapi_test_project.com/contact/",
+        "email": "klaipher@fastapi_test_project.com",
     },
     license_info={
         "name": "The Unlicense",
